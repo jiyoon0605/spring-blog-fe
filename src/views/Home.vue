c0e6cb6350a85ae24831a040607ecd4c62ea47f6
jiyoon <jylee@crscube.co.kr>
2021-11-08 14:04:58 +0900

    deps setting

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    Home
+    <router-view></router-view>
+  </div>
+</template>
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
+
+@Component
+export default class Home extends Vue{
+
+}
+</script>
